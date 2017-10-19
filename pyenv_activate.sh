4cdf9326c2ef3ff81d4fb8131430b2fa42b1c35a
ngroup <steven.nien@gmail.com>
2017-10-19 12:23:50 +0800

    fix: matplotlib

--- pyenv_activate.sh
+++ pyenv_activate.sh
@@ -4,7 +4,10 @@ pyenv update
 pyenv install 3.6.3
 pyenv virtualenv 3.6.3 science
 pyenv global science
+exec "$SHELL"
 pyenv activate science
 pip install -U setuptools
 pip install -U pip
 pip install numpy scipy matplotlib ipython sympy pandas
+touch  ~/.config/matplotlib/matplotlibrc
+echo 'backend : Agg' >> ~/.config/matplotlib/matplotlibrc
