1d62d7456437d9dd6db8b9ee4ccc0903d558e9cb
ngroup <steven.nien@gmail.com>
2017-10-19 15:21:12 +0800

    fix: path missing

--- pyenv_activate.sh
+++ pyenv_activate.sh
@@ -7,5 +7,5 @@ pyenv global science
 pip install -U setuptools
 pip install -U pip
 pip install numpy scipy matplotlib ipython sympy pandas networkx tiny2d
-touch  ~/.config/matplotlib/matplotlibrc
+mkdir -p ~/.config/matplotlib/ && touch  ~/.config/matplotlib/matplotlibrc
 echo 'backend : Agg' >> ~/.config/matplotlib/matplotlibrc
