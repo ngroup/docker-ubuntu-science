2b751940402a54e51ad2ab289eaf6ff3f2112352
ngroup <steven.nien@gmail.com>
2017-10-19 13:32:40 +0800

    fix: pyenv

--- pyenv_activate.sh
+++ pyenv_activate.sh
@@ -4,10 +4,8 @@ pyenv update
 pyenv install 3.6.3
 pyenv virtualenv 3.6.3 science
 pyenv global science
-exec "$SHELL"
-pyenv activate science
 pip install -U setuptools
 pip install -U pip
-pip install numpy scipy matplotlib ipython sympy pandas
+pip install numpy scipy matplotlib ipython sympy pandas networkx tiny2d
 touch  ~/.config/matplotlib/matplotlibrc
 echo 'backend : Agg' >> ~/.config/matplotlib/matplotlibrc
